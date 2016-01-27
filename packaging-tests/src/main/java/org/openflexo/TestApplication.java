4e7fcaef33a66e271b75d47b672175ee8c82a1fd
xtof <christophe@openflexo.org>
2016-01-27 11:45:11 +0100

    LOW: small fix for packaging-tests

---
+++ packaging-tests/src/main/java/org/openflexo/TestApplication.java
@@ -0,0 +1,12 @@
+package org.openflexo;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println( "Tada!!");
+		
+	}
+
+}
